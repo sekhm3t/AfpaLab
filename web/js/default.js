e09da7693ffa637cde3a0c084f3efcbb4631d506
GaelWLR <fRm-VXM-mC9-hNC>
2018-04-10 16:44:52 +0200

    Responsive NAV

--- web/js/default.js
+++ web/js/default.js
@@ -1,3 +1,19 @@
+/************************************************************/
+/*							Nav								*/
+/************************************************************/
+
+// Moving the cross
+$(function() {
+	$("body").on("click touchstart", "#burger__container", function() {
+		this.classList.toggle("change");
+		$("#nav__div__links").toggle();
+	})
+});
+
+/************************************************************/
+/*							Modals							*/
+/************************************************************/
+
 // Open the login modal
 function open__login() {
 	$("#modal__login").toggle();
@@ -5,11 +21,18 @@ function open__login() {
 
 // Open the contact modal
 function open__contact(user, key_user) {
-	$("#recipient").html("Destinataire: " + user);
+	$("#contact__recipient").html("Destinataire: " + user);
 	$("#modal__contact").toggle();
 }
 
-// Close the login modal by clicking anywhere
+// Close the modal by clicking on the close btn
+$(function() {
+	$("body").on("click touchstart", ".close__modal", function() {
+		$(".modal").hide();
+	})
+});
+
+// Close the modal by clicking anywhere
 window.onclick = function (ev) {
 	let modals = ev.target === document.getElementById("modal__login") || ev.target === document.getElementById("modal__contact");
 	if (modals) {
