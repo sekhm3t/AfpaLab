40aa8d3ddac7e7a8ef792a940440bc4e98235e93
GaelWLR <fRm-VXM-mC9-hNC>
2018-04-26 18:08:24 +0200

    Update

---
+++ web/js/select_session.js
@@ -0,0 +1,69 @@
+$("select").on("change", function selectSession(){
+	
+	var datas = 
+	{
+		id_formation: 2,
+		bJSON: 1,
+		page: "SelectSession"
+	}
+
+	$.ajax(
+	{
+		type: "POST",
+		url: "route.php",
+		async: false,
+		data: datas,
+		dataType: "json",
+		cache: false,
+	})
+
+	.done(function(result) 
+	{
+
+		$.each(result, function () {
+		   $.each(this, function (name, value) {
+		      console.log(name + '=' + value);
+		   });
+	})
+
+	.fail(function(result)
+	{
+		alert("Echec de la requête !");
+
+	});
+})
+	
+
+function selectStagiaire(){
+	
+	var datas = 
+	{
+		id_session: id_session,
+		bJSON: 1,
+		page: "selectStagiaire"
+	}
+
+	$.ajax(
+	{
+		type: "POST",
+		url: "route.php",
+		async: false,
+		data: datas,
+		dataType: "json",
+		cache: false,
+	})
+
+	.done(function(result) 
+	{
+
+		console.log(result);
+
+		
+	})
+
+	.fail(function(result)
+	{
+		alert("Echec de la requête !");
+
+	});
+}
